214cda32f0d494baab443e29b35151e56cb4f8e2
admintla <larochethibaut@gmail.com>
2026-04-27 20:47:03 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-fetch("https://hcm55preview.sapsf.eu/sf/logout?bplte_company=LVMHTT18", { mode: "no-cors" })
+fetch("https://hcm55preview.sapsf.eu/sf/logout?bplte_company=", { mode: "no-cors" })
   .then(function() {
     return new Promise(function(resolve) {
       setTimeout(resolve, 1000);
     });
   })
   .then(function() {
-    window.open("https://azjvwrftk.accounts.ondemand.com/saml2/idp/sso?sp=https://www.successfactors.com/LVMHTT6&idp=https://azjvwrftk.accounts.ondemand.com", "_blank");
+    window.open("https://", "_blank");
   });
